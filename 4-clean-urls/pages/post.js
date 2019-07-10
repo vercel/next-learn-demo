1684e00ed74dc90d015fab1881aade1adbca34af
Luis Alvarez <luis@zeit.co>
2019-07-10 11:44:25 -0500

    Updated the clean-urls demo

--- 4-clean-urls/pages/post.js
+++ 4-clean-urls/pages/post.js
@@ -1,13 +1,15 @@
-import { withRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import Layout from '../components/MyLayout.js'
 
-function Post(props) {
+const Page = () => {
+  const router = useRouter()
+
   return (
     <Layout>
-      <h1>{props.router.query.title}</h1>
+      <h1>{router.query.title}</h1>
       <p>This is the blog post content.</p>
     </Layout>
   )
 }
 
-export default withRouter(Post)
+export default Page
