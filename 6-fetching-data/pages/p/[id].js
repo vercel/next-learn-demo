b51639377635752e0b595b9163d322bcba17539e
Mohammed Affan <mohammed.affan4@gmail.com>
2019-07-19 11:51:25 +0530

    Error in your p/[id] code in the learn demo in website,so resolved it

--- 6-fetching-data/pages/p/[id].js
+++ 6-fetching-data/pages/p/[id].js
@@ -1,13 +1,24 @@
-import { useRouter } from 'next/router'
 import Layout from '../../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
 
-export default function Post() {
-  const router = useRouter()
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </Layout>
+)
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  )
+Post.getInitialProps = async function(context) {
+  // console.log(context.asPath.slice(3))
+  const id = context.asPath.slice(3)
+  console.log(id)
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
 }
+
+export default Post
